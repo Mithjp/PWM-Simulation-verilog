4adbd8b8ec6671967041b86161ef38bfe0d28773
Mithun Jayaprakash <129208089+Mithjp@users.noreply.github.com>
2024-09-17 23:14:45 +0530

    Add files via upload

---
+++ pwm.v
@@ -0,0 +1,20 @@
+module pwm(
+    input clk,
+    output reg[3:0] pwm_out
+);
+    reg[6:0] cntr=0;
+    always @(posedge clk)
+    begin
+        if(cntr < 100)
+        begin
+            cntr <= cntr +1;
+            pwm_out[0] <= (cntr < 25) ? 1'b1 : 1'b0;
+            pwm_out[1] <= (cntr < 50) ? 1'b1 : 1'b0;
+            pwm_out[2] <= (cntr < 75) ? 1'b1 : 1'b0;
+            pwm_out[3] <= (cntr < 95) ? 1'b1 : 1'b0;
+         end
+         else
+             cntr <=0;
+    end
+endmodule
+            
